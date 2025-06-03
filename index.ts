9a9e009ce3ce105800f92ba7d718bf8a9fb7c346
Ewann ROUX <ewann.roux@recia.fr>
2025-06-03 11:26:29 +0200

    feat: export functions in index.ts

---
+++ index.ts
@@ -0,0 +1,10 @@
+import { getDocumentsPublisher } from './src/services/documentsPublisherService'
+import { getFavorisMediacentre } from './src/services/favorisMediacentreService'
+import { getFavorisPortail } from './src/services/favorisPortailService'
+import 'regenerator-runtime/runtime.js'
+
+export {
+  getDocumentsPublisher,
+  getFavorisMediacentre,
+  getFavorisPortail,
+}
