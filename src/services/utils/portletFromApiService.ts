1f70734c10a4b410e0ca8c0d3cb10bbfd99ba759
Ewann ROUX <ewann.roux@recia.fr>
2025-06-09 09:56:00 +0200

    feat: create json with more informations for widgets

---
+++ src/services/utils/portletFromApiService.ts
@@ -0,0 +1,49 @@
+/**
+ * Copyright (C) 2025 GIP-RECIA, Inc.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+export default class portletFromApiService {
+  static getUrl(portlet: any): string {
+    // TODO : put '/p/' in conf
+    return portlet?.parameters?.alternativeMaximizedLink
+      ? portlet.parameters.alternativeMaximizedLink
+      : `/portail/p/${portlet.fname}`
+  }
+
+  static getTarget(portlet: any): string {
+    if (getAlternativeMaximizedUrl(portlet)) {
+      return getAlternativeMaximizedTarget(portlet)
+    }
+    return '_self'
+  }
+
+  static getRel(portlet: any): string {
+    return hasAlternativeMaximizedUrl(portlet)
+      ? 'noopener noreferrer'
+      : ''
+  }
+}
+
+function hasAlternativeMaximizedUrl(portlet: any): string {
+  return getAlternativeMaximizedUrl(portlet)
+}
+
+function getAlternativeMaximizedTarget(portlet: any): string {
+  return portlet?.parameters?.alternativeMaximizedLinkTarget ?? '_blank'
+}
+
+function getAlternativeMaximizedUrl(portlet: any): string {
+  return portlet?.parameters?.alternativeMaximizedLink
+}
