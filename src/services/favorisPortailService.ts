b623c3d04a357073e6bc63ca851e42a1a30aa12e
Quentin Guillemin <quentin.guillemin@recia.fr>
2025-09-18 11:46:05 +0200

    fix: introduced bug due to refactor
    11da35c5de7c6bfc003db98897e9a51ae8e0006b

--- src/services/favorisPortailService.ts
+++ src/services/favorisPortailService.ts
@@ -15,7 +15,6 @@
  */
 
 import type { Item } from '../types/Item'
-import portletFromApiService from './portletFromApiService.ts'
 import { getRegistryPortletsArray } from './registryService.ts'
 import fetchFavorites from './utils/fetchFavorites.ts'
 import byFavoriteOrder from './utils/sortByFavoriteOrder.ts'
@@ -34,10 +33,10 @@ async function getFavorisPortail(): Promise<string> {
   favoritesSortedAndFiltered.forEach((value: any, _index: number) => {
     const favoriteAsItem: Item = {
       name: value.title,
-      link: portletFromApiService.getUrl(value),
+      link: getUrl(value),
       icon: value.parameters.iconUrl ? value.parameters.iconUrl.value : '',
-      target: portletFromApiService.getTarget(value),
-      rel: portletFromApiService.getRel(value),
+      target: getTarget(value),
+      rel: getRel(value),
       event: '',
       eventpayload: '',
       eventDNMA: 'click-portlet-card',
@@ -60,6 +59,44 @@ function flattenFavorites(elem: any): Array<any> {
     return [elem]
 
   return []
+}
+
+function getUrl(portlet: any): string {
+  return portlet?.parameters?.alternativeMaximizedLink?.value
+    ? portlet.parameters.alternativeMaximizedLink.value
+    : `${getConfig().global.context}/p/${portlet.fname}`
+}
+
+function getTarget(portlet: any): string {
+  if (getAlternativeMaximizedUrl(portlet)) {
+    return getAlternativeMaximizedTarget(portlet)
+  }
+  return '_self'
+}
+
+function getRel(portlet: any): string {
+  return hasAlternativeMaximizedUrl(portlet)
+    ? 'noopener noreferrer'
+    : ''
+}
+
+function hasAlternativeMaximizedUrl(portlet: any): string {
+  return getAlternativeMaximizedUrl(portlet)
+}
+
+function getAlternativeMaximizedTarget(portlet: any): string {
+  return portlet?.parameters?.alternativeMaximizedLinkTarget?.value ?? '_blank'
+}
+
+function getAlternativeMaximizedUrl(portlet: any): string {
+  return portlet?.parameters?.alternativeMaximizedLink?.value
+}
+
+function getConfig(): { global: GlobalConfig, favoris: FavorisConfig } {
+  return {
+    global: window.WidgetAdapter.config.global,
+    favoris: window.WidgetAdapter.config.favoris,
+  }
 }
 
 export {
