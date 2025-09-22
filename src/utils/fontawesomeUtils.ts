2928b6943517e55caf98ce15a77cc2f04fbf828e
Quentin Guillemin <quentin.guillemin@recia.fr>
2025-09-22 17:19:43 +0200

    feat: use fontawsome

---
+++ src/utils/fontawesomeUtils.ts
@@ -0,0 +1,49 @@
+/**
+ * Copyright (C) 2025 GIP-RECIA, Inc.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+import type { IconDefinition } from '@fortawesome/fontawesome-svg-core'
+
+function getPath(data: string): string {
+  return `<path fill="currentColor" d="${data}"></path>`
+}
+
+function getIconWithStyle(svgDefinition: IconDefinition, styles: string[] = [], classes: string[] = []): string {
+  const { prefix, iconName, icon: [width, height, , , pathData] } = svgDefinition
+
+  return `
+      <svg
+        style="${styles.join(' ')}"
+        aria-hidden="true"
+        focusable="false"
+        data-prefix="${prefix}"
+        data-icon="${iconName}"
+        class="${classes.join(' ')} svg-inline--fa fa-${iconName}"
+        role="img"
+        xmlns="http://www.w3.org/2000/svg"
+        viewBox="0 0 ${width} ${height}"
+      >
+        ${
+          typeof pathData !== 'string'
+            ? pathData.map(data => getPath(data))
+            : getPath(pathData)
+        }
+      </svg>
+    `
+}
+
+export {
+  getIconWithStyle,
+}
